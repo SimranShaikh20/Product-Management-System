4fb6c5395503da249bc0b0300b22338f5caca976
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-02-01 11:00:50 +0530

    commit

---
+++ ProductManagementSystem/Data/ProductManagmentSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductManagmentSystem.Models;
+
+namespace ProductManagmentSystem.Data
+{
+    public class ProductManagmentSystemContext : DbContext
+    {
+        public ProductManagmentSystemContext (DbContextOptions<ProductManagmentSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductManagmentSystem.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ ProductManagementSystem/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManagmentSystem.Models
+{
+    public class Product
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+    }
+}
